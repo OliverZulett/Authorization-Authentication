95dd808a51d964285d44f54e21a164ada3175a45
ozulett <oliverzulett@gmail.com>
2022-02-02 22:57:33 -0400

    access token

--- src/resources/auth/auth.module.ts
+++ src/resources/auth/auth.module.ts
@@ -4,12 +4,31 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from '../../persistance/repositories/user.repository';
 import { BcryptService } from '../../shared/services/bcrypt/bcrypt.service';
+import { JwtModule } from '@nestjs/jwt';
+import { join } from 'path';
+import * as fs from 'fs'
+
+const PRIVATE_KEY = fs.readFileSync(
+  join(process.cwd(), '/certs/keypair.pem')
+);
+
+const PUBLIC_KEY = fs.readFileSync(
+  join(process.cwd(), '/certs/publickey.crt')
+);
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       UserRepository
-    ])
+    ]),
+    JwtModule.register({
+      privateKey: PRIVATE_KEY,
+      publicKey: PUBLIC_KEY,
+      signOptions: {
+        expiresIn: '1h',
+        algorithm: 'RS256',
+      }
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService, BcryptService]
--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -9,6 +9,8 @@ import { UserRepository } from '../../persistance/repositories/user.repository';
 import { UserSingInDto } from './dto/user-singIn.dto';
 import { UserSingUpDto } from './dto/user-singUp.dto';
 import { BcryptService } from '../../shared/services/bcrypt/bcrypt.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from './models/user.model';
 
 @Injectable()
 export class AuthService {
@@ -17,22 +19,22 @@ export class AuthService {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly bcryptService: BcryptService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async signIn(userSingInDto: UserSingInDto) {
-    const userFound = await this.userRepository.findUserByEmail(userSingInDto.email);
+    const userFound = await this.userRepository.findUserByEmail(
+      userSingInDto.email,
+    );
     if (!userFound) {
       throw new NotFoundException(`User dosen't exists`);
     }
-    let passwordIdValid = false;
-    try {
-      passwordIdValid = await this.bcryptService.isMatch(userSingInDto.password, userFound.hash);
-    } catch (error) {
-      this.logger.error(error);
-      throw new InternalServerErrorException('Error validating password with bcrypt')
-    }
-    if (passwordIdValid) {
-      return true;
+    const passwordIsValid = await this.passwordIsValid(
+      userSingInDto.password,
+      userFound.hash,
+    );
+    if (passwordIsValid) {
+      return this.getAccessToken(userFound);
     }
     throw new BadRequestException('Invalid password');
   }
@@ -47,18 +49,50 @@ export class AuthService {
     if (await this.userRepository.findUserByEmail(userSingUpDto.email)) {
       throw new BadRequestException('Email already exists');
     }
-    let passwordHashed;
+    const passwordHashed = await this.hashPassword(userSingUpDto.password);
     try {
-      passwordHashed = await this.bcryptService.getHash(userSingUpDto.password);
-    } catch (error) {
-      this.logger.error(error);
-      throw new InternalServerErrorException('Error hashing the password')
-    }
-    try {
-      return await this.userRepository.save({...userSingUpDto, hash: passwordHashed});
+      const userCreated = await this.userRepository.save({
+        ...userSingUpDto,
+        hash: passwordHashed,
+      });
+      return this.getAccessToken(userCreated);
     } catch (error) {
       this.logger.error(error);
       throw new InternalServerErrorException('Error creating new user in DB');
+    }
+  }
+
+  private async hashPassword(password: string) {
+    try {
+      return await this.bcryptService.getHash(password);
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException('Error hashing the password');
+    }
+  }
+
+  private async passwordIsValid(password: string, hash: string) {
+    try {
+      return await this.bcryptService.isMatch(password, hash);
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException(
+        'Error validating password with bcrypt',
+      );
+    }
+  }
+
+  private getAccessToken(user: User) {
+    try {
+      const accessToken = this.jwtService.sign({
+        user_id: user.id,
+        user_name: user.username,
+        user_email: user.email,
+      });
+      return { access_token: accessToken };
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException('Error creating access token');
     }
   }
 }
